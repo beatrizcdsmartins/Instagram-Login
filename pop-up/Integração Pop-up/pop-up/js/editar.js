ef732aab6796eb5ef110168a59bbf2cea764792a
beatrizcdsmartins <beatrizcdsmartins@gmail.com>
2023-06-09 12:40:23 -0300

    pop-up

---
+++ pop-up/Integração Pop-up/pop-up/js/editar.js
@@ -0,0 +1,17 @@
+      bioEp.init({
+            html: '<div id="bio_ep_content">' +
+            '<span></span>' +
+            '<span></span>' +
+            '<span></span>' +
+            '</div>',
+            css: '#bio_ep {width: 400px; height: 400px; color: #333; background-color: transparent; text-align: center;background-image:url(./../../../../assets/popUpImage.png);background-size: cover;}' +
+            '#bio_ep_content {padding: 255px 0 0 0; font-family: inherit;}' +
+            '#bio_ep_content span:nth-child(2) {display: block; color: #f21b1b; font-size: 32px; font-weight: 600;}' +
+            '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
+            '#bio_ep_content span:nth-child(4) {display: block; margin: -5px 0 0 0; font-size: 16px; font-weight: 600;}' +
+            '.bio_btn {display: inline-block; margin: 18px 0 0 0; padding: 8px 25px 8px 25px; color: #fff; font-size: 19px; font-weight: 900; background-color: #ff4719; border: 1px solid #fff; cursor: pointer; -webkit-appearance: none; -moz-appearance: none; border-radius: 100px; text-decoration: none;}',
+            cookieExp: 0
+        });
+
+        document.getElementById('./../../../../assets/')
+        
